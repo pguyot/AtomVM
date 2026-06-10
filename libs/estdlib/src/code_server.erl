bf8619e7faa8ace2b1969c1de3e135ef14d35a2a
Paul Guyot <pguyot@kallisys.net>
2026-06-10 22:34:59 +0200

    code_server: print JIT compilation messages to standard_error
    Runtime JIT compilation timing is diagnostic output; an escript's stdout
    should only carry the program's own output.
    
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- libs/estdlib/src/code_server.erl
+++ libs/estdlib/src/code_server.erl
@@ -182,7 +182,7 @@ load(Module) ->
             {Pid, Ref} = spawn_opt(
                 fun() ->
                     try
-                        io:format("Compilation of ~s...", [Module]),
+                        io:format(standard_error, "Compilation of ~s...", [Module]),
                         Start = erlang:system_time(millisecond),
                         Code = code_server:code_chunk(Module),
                         AtomResolver = fun(Index) -> code_server:atom_resolver(Module, Index) end,
@@ -214,12 +214,19 @@ load(Module) ->
                         Stream2 = StreamModule:flush(Stream1),
                         code_server:set_native_code(Module, LabelsCount, Stream2),
                         End = erlang:system_time(millisecond),
-                        io:format("~B ms (bytecode: ~B bytes, native code: ~B bytes)\n", [
-                            End - Start, byte_size(Code), BackendModule:offset(BackendState1)
-                        ])
+                        io:format(
+                            standard_error,
+                            "~B ms (bytecode: ~B bytes, native code: ~B bytes)\n",
+                            [
+                                End - Start,
+                                byte_size(Code),
+                                BackendModule:offset(BackendState1)
+                            ]
+                        )
                     catch
                         T:V:S ->
                             io:format(
+                                standard_error,
                                 "===========================================================\nJust in time compilation of module ~p failed.\n~p:~p\n~p\n===========================================================\n",
                                 [Module, T, V, S]
                             ),
