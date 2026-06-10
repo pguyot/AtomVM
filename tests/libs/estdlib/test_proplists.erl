08c92eb93177d554e9a462dbca8a1880657abd38
Paul Guyot <pguyot@kallisys.net>
2026-06-10 19:01:29 +0200

    estdlib: add proplists substitute_aliases, substitute_negations, expand, normalize
    beam_ssa_opt normalizes its options with proplists:normalize(Opts,
    [{negations, Negations}]).
    
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- tests/libs/estdlib/test_proplists.erl
+++ tests/libs/estdlib/test_proplists.erl
@@ -24,6 +24,65 @@
 
 test() ->
     ok = test_get_value(),
+    ok = test_substitute_aliases(),
+    ok = test_substitute_negations(),
+    ok = test_expand(),
+    ok = test_normalize(),
+    ok.
+
+test_substitute_aliases() ->
+    ok = etest:assert_match(
+        proplists:substitute_aliases([{color, colour}], [{color, red}, color, size]),
+        [{colour, red}, colour, size]
+    ),
+    ok = etest:assert_match(proplists:substitute_aliases([], [a, {b, 1}]), [a, {b, 1}]),
+    ok.
+
+test_substitute_negations() ->
+    ok = etest:assert_match(
+        proplists:substitute_negations(
+            [{no_foo, foo}], [no_foo, {no_foo, true}, {no_foo, false}, bar]
+        ),
+        [{foo, false}, {foo, false}, foo, bar]
+    ),
+    ok.
+
+test_expand() ->
+    ok = etest:assert_match(
+        proplists:expand([{fast, [{speed, 9}, low_mem]}], [slim, fast, low]),
+        [slim, {speed, 9}, low_mem, low]
+    ),
+    % a non-matching minimal representation is kept as is
+    ok = etest:assert_match(
+        proplists:expand([{{foo, true}, [bar]}], [fie, {foo, false}, fum]),
+        [fie, {foo, false}, fum]
+    ),
+    % later entries with the same key are deleted on expansion
+    ok = etest:assert_match(
+        proplists:expand([{foo, [bar]}], [fie, foo, fum, foo]),
+        [fie, bar, fum]
+    ),
+    ok.
+
+test_normalize() ->
+    % the form beam_ssa_opt uses
+    ok = etest:assert_match(
+        proplists:normalize(
+            [no_copt, {inline, true}, verbose], [{negations, [{no_copt, copt}]}]
+        ),
+        [{copt, false}, inline, verbose]
+    ),
+    ok = etest:assert_match(
+        proplists:normalize(
+            [{color, red}, no_foo],
+            [
+                {aliases, [{color, colour}]},
+                {negations, [{no_foo, foo}]},
+                {expand, [{colour, [paint]}]}
+            ]
+        ),
+        [{colour, red}, {foo, false}]
+    ),
     ok.
 
 test_get_value() ->
